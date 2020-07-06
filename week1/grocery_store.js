428abb5e46957832053e0414f3a043f768483e08
Kerry hHampton <65149075+jessekerry@users.noreply.github.com>
2020-07-05 23:47:04 -0700

    first-commit

---
+++ week1/grocery_store.js
@@ -0,0 +1,23 @@
+var shopper = {
+    firstName: "odin",
+    lastName: "hampton",
+    sex: "male",
+    age: 40,
+    isAlone: false,
+    cart: candy, iceCream, milk,
+    fullName : function(){
+        return this.firstName + " " + this.lastName;
+    }
+};
+var item = {
+    name: candy,
+    price: 25,
+}
+var item = {
+    name: iceCream,
+    price: 10,
+}
+var item = {
+    name: milk,
+    price: 10,
+}
