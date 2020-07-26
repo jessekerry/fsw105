d1fcf591d6cc8d67097a03a73d1469162204504e
Kerry hHampton <65149075+jessekerry@users.noreply.github.com>
2020-07-26 11:17:13 -0700

    fifth-commit

---
+++ week3/functions.js
@@ -0,0 +1,39 @@
+function sum (num1, num2){
+    return(num1 + num2)
+}
+var outcome = sum (58,23)
+console.log(outcome)
+
+
+function highest (num1, num2, num3){
+    if (num1 > num2 && num1 > num3){
+        console.log(num1)
+    } else if (num2 > num1 && num2 > num3) {
+        console.log(num2)
+    } else {
+        console.log(num3)
+    } 
+}
+highest(14, 155, 88)
+
+
+function singles (num){
+    if (num %2 === 0){
+        console.log ("even")
+    } else {
+        console.log ("odd")
+    }    
+}
+singles(21)
+
+var val = "abcdefghijklmnopqrst";
+var bear = "ABCDEFGHIJKLMNOPQRSTU";
+
+function jack (info){
+    if (info.length <= 20){
+        console.log((info) + (info))
+    } else {
+        console.log ((info).slice(0, info.length / 2))
+    }
+} 
+jack(val)
---
+++ week3/loops_and_arrays.js
@@ -0,0 +1,67 @@
+var attendees = [
+    {
+        name: "Mike",
+        age: 12,
+        gender: "male"
+    }, {
+        name: "Madeline",
+        age: 80,
+        gender: "female"
+    }, {
+        name: "cheryl",
+        age: 22,
+        gender: "female"
+    }, {
+        name: "sam",
+        age: 30,
+        gender: "male"
+    }, {
+        name: "suzy",
+        age: 4,
+        gender: "female"
+    }
+]
+function bella (attendees){
+    for (i = 0; i < attendees.length; i++){
+        if (attendees[i].age >= 18){
+            console.log("is old enough")
+        }else{
+            console.log("is not old enough")
+        }
+    }
+}
+
+function odin (attendees){
+    for (i = 0; i < attendees.length; i++){
+        if (attendees[i].age >= 18){
+            console.log(`${attendees[i].name} is old enough to see Mad Max`)
+        } else {
+            console.log(`${attendees[i].name} is not old enough to see Mad max`)
+        }   
+    }
+}
+function nana (attendees){
+    for (i = 0; i < attendees.length; i++){
+        if (attendees[i].gender === "male" && attendees[i].age >= 18){
+            console.log (`${attendees[i].name} is old enough to see Mad Max let him in`)
+        } else if (attendees[i].gender === "female" && attendees[i].age >= 18){
+        console.log (`${attendees[i].name} is old enough to see Mad Max let her in`)
+        } else if (attendees[i].gender === "male" && attendees[i].age < 18){
+        console.log (`${attendees[i].name} is not old enough to see Mad Max do not  let him in`)
+        } else {
+            console.log (`${attendees[i].name} is not old enough to see Mad Max do not let her in`)
+        }
+    }
+}
+console.log( (attendees))
+
+for (i = 0; i < 101; i++){
+    if([i] % 2 != 0){
+        console.log([i] + "odd")
+    } else {
+        console.log([i] + "even")
+    }
+}
+
+
+
