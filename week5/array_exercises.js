ab4eeffc753a84ac0a849977bec9876283cb44e0
Kerry hHampton <65149075+jessekerry@users.noreply.github.com>
2020-09-30 21:21:40 -0700

    seventh-commit

--- week5/array_exercises.js
+++ week5/array_exercises.js
@@ -1,102 +1,35 @@
-// Section A # 1
-const { keyIn } = require("readline-sync");
-{
-let nums = [3, 6, 8, 2];
-function fiveAndGreaterOnly(num) {
-  return num >= 5;
-}
-results = nums.filter(fiveAndGreaterOnly);
-console.log(results, "A1")
-
-
-
-
-// Section A # 2
-function evensOnly(num) {
-    return num %2 === 0;
-}
-evens = nums.filter(evensOnly);
-console.log(evens, "A2")
-}
-
-
-
-
-// Section A Extra Credit
-{
-    let people = [
     {
-        name: "Angelina Jolie",
-        age: 80
-    }, {
-        name: "Eric Jones",
-        age: 2
-    }, {
-        name: "Paris Hilton",
-        age: 5
-    }, {
-        name: "Kayne West",
-        age: 16
-    }, {
-        name: "Bob Ziroll",
-        age: 100
+    function fiveAndGreaterOnly(arr) {
+        const result = arr.filter(function (num) {
+            if (num >=5) {
+                return true;
+            };
+        });
+        return result;
+    };
+    
+    console.log(" A 1 ", fiveAndGreaterOnly([3, 6, 8, 2]))
     }
-]
-function ofAge(people){
-    return people.age >= 17
-}
-eligibility = people.filter(ofAge);
-// console.log(people)
-console.log(eligibility, "A.ec")
-}
-
-
-
-
-
-// Section B #1
-{
-    const arr = [2, 5, 100];
-    const result = arr.map(function(num){
-        return  num * 2    
-    })
-    console.log(arr, "B1")
-    console.log(result, "B1")
-}
-
-
-
-
-
-// Sectio B # 2
-{
-    const arr = [2, 5, 100];
-    const result = arr.map(function(num){
-        return     num.toString()
-    })
-    // console.log(arr)
-    console.log(result, "B2")
-}
-
-
-
-
-
-// Section B # 3
- {
-    const str = ["john", "jacob", "jinGleHeimer", "schmidt"];
-    const toCapital = str => {
-      const words = str;
-      return words.map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())};
-    console.log(toCapital(str), "B3")   
-} 
-
-
-
-
-// Section B Extra credit 1
-{
-    let people = [
+    
+    
+    
+    {
+    function evensOnly(arr) {
+        const outcome = arr.filter(function(num){
+            if (num % 2 == 0) {
+                return true;
+            };
+        });
+        return outcome
+    }
+   
+    console.log(" A 2 ", evensOnly([3, 6, 8, 2]))
+    }
+    
+    
+    
+    {
+        let people = [
         {
             name: "Angelina Jolie",
             age: 80
@@ -114,117 +47,224 @@ console.log(eligibility, "A.ec")
             age: 100
         }
     ]
-    const justNames = people.map(function(people) {
-        return people.name 
-    });
-    console.log(justNames, "B.ec1")
-    
-    
-    
-    
-    // Section B Extra credit # 2    NEED TO  CHANGE TO  .map()  TO MEET ASSIGNMENT REQUIREMENTS !!!!
-   function allowed (arr){ 
-    const justNames = arr.map((arr) => arr.age >= 17 ? (`${arr.name} can go to the Matrix`) : (`${arr.name} is underaged!!`));
-    console.log(justNames)
+   
+    function ofAge(people){
+        return people.age >= 17
     }
-console.log(allowed(people), "B.ec2");
-}
-
-
-
-
-// Section C # 1
-{
-const nums = [1, 2, 3]
-const sum = nums.reduce((total, num) => (total + num), 0);
-console.log(sum, "C1")
-// Section C # 2
-const change = nums.reduce((total, nums) => total.concat(nums), "");
-console.log(change, "C2")
-} 
-
-
-
-
-// Section C # 3 
-{
- totalVotes = (arr) => arr.reduce((total, voter) => voter.voted ? total + 1 :(total), 0);
-const voters = [
-    {name: "Bob" , age: 30 , voted: true},
-    {name: "Jake" , age: 32 , voted: true},
-    {name: "Kate" , age: 25 , voted: false},
-    {name: "Sam" , age: 20 , voted: false},
-    {name: "Phil" , age: 21 , voted: true},
-    {name: "Ed" , age: 55 , voted: true},
-    {name: "Tami" , age: 54 , voted: true},
-    {name: "Mary" , age: 31 , voted: false},
-    {name: "Becky" , age: 43 , voted: false},
-    {name: "Joey" , age: 41 , voted: true},
-    {name: "Jeff" , age: 30 , voted: true},
-    {name: "Zack" , age: 19 , voted: false},
-];
- console.log(totalVotes(voters), "C3");
-}
-
-
-
-
-// Section C Extra Credit # 1 
-{
-const wishlist = [ 
-    {title: "Tesla Model S", price: 90000 },
-    {title: "4 carat diamond ring", price: 45000 },
-    {title: "Fancy hacky sack", price: 5 },
-    {title: "Gold fidgit spinner", price: 2000 },
-    {title: "A second Tesla Model S", price: 90000 }
-];
-const totalPrices = wishlist.reduce((total, item) => total + item.price, 0);
-console.log(totalPrices, "C.ec1")
-}
-
-
-
-
-// Section C Extra Credit # 2
-{
-const arrays = [
-["1", "2", "3"],
-[true],
-[4, 5, 6]
-];
-const arraysFlat = arrays.flat();
-console.log(arraysFlat, "C.ec2")
-}
-
-
-
-
-// Section D # 1
-{
-const nums = [1, 3, 5, 2, 90, 20];
-const leastToGreatest = nums.sort((a, b) =>(a > b ? 1 : -1));
-console.log(leastToGreatest, "D1")
-
-
-
-
-
-// Section D # 2
-const greatestToLeast = nums.sort((a, b) => (a < b ? 1 : -1));
-console.log(greatestToLeast, "D2")
-}
-
-
-
-// Section D # 3
-{
-const sos = ["dog", "wolf", "by", "family", "eaten"];
-const shortToLong = sos.sort((a, b) => (a.length - b.length));
-console.log(shortToLong, "D3")
-
-
-
-// Section D Extra Credit 
-sos.sort();
-console.log(sos, "D.ec")
-}
+   
+    eligibility = people.filter(ofAge);
+    
+    // console.log(people)
+    
+    console.log(" A extra credit ", eligibility)
+    }
+    
+    
+    
+    {
+        const arr = [2, 5, 100];
+        const result = arr.map(function(num){
+            return  num * 2
+        })
+      
+        // console.log(arr)
+       
+        console.log(" B 1 ", result)
+    
+    
+    
+    
+    
+        const result = arr.map(function(num){
+            return     num.toString()
+        })
+       
+        // console.log(arr)
+       
+        console.log(" B 2 ", result)
+    }
+    
+    
+    
+    {
+        const str = ["john", "jacob", "jinGleHeimer", "schmidt"];
+       
+        const toCapital = str => {
+          const words = str;
+          return words.map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())};
+        
+          console.log(" B 3 ", toCapital(str))
+        }
+    
+    
+    
+        {
+        let people = [
+            {
+                name: "Angelina Jolie",
+                age: 80
+            }, {
+                name: "Eric Jones",
+                age: 2
+            }, {
+                name: "Paris Hilton",
+                age: 5
+            }, {
+                name: "Kayne West",
+                age: 16
+            }, {
+                name: "Bob Ziroll",
+                age: 100
+            }
+        ]
+        
+        function namesOnly(arr) {
+            const justNames = arr.map(function(arr) {
+                return arr.name
+            });
+            return justNames
+        };
+       
+        console.log(" B extra credit 1 ", namesOnly(people))
+     
+     
+     
+        function makeStrings(arr) {
+         const allowed = arr.map((arr) => arr.age >= 17 ? (` ${arr.name} can go to the Matrix`) : (` ${arr.name} is underage!!`));
+            return allowed
+     }
+      
+     console.log(" B extra credit 2 ", makeStrings(people))
+    
+    }
+    
+    
+    
+    function totals(arr) {
+        const endTotal = arr.reduce(function(final, num) {
+            final = final + num;
+            return final
+        });
+        return endTotal;
+    }
+    console.log(" C 1 ", totals([1, 2, 3]))
+    
+    
+    
+    function stringConcat(arr) {
+        const change = arr.reduce(function(string, num){
+            return string + num.toString()
+        })
+        return change
+    }
+    console.log(" C 2 ", stringConcat([1, 2, 3]))
+    
+    
+    
+    let voters = [
+        {name: "Bob" , age: 30 , voted: true},
+        {name: "Jake" , age: 32 , voted: true},
+        {name: "Kate" , age: 25 , voted: false},
+        {name: "Sam" , age: 20 , voted: false},
+        {name: "Phil" , age: 21 , voted: true},
+        {name: "Ed" , age: 55 , voted: true},
+        {name: "Tami" , age: 54 , voted: true},
+        {name: "Mary" , age: 31 , voted: false},
+        {name: "Becky" , age: 43 , voted: false},
+        {name: "Joey" , age: 41 , voted: true},
+        {name: "Jeff" , age: 30 , voted: true},
+        {name: "Zack" , age: 19 , voted: false},
+    ];
+    
+    function totalVotes(arr) {
+        const voteCount = arr.reduce(function(final, voter) {
+            if (voter.voted) {
+                final++
+            }else{
+                final
+            }
+            return final
+        }, 0)
+        return voteCount
+    
+    
+    console.log(" C 3 ", totalVotes(voters));
+    }
+    
+    
+    const wishlist = [
+        {title: "Tesla Model S", price: 90000 },
+        {title: "4 carat diamond ring", price: 45000 },
+        {title: "Fancy hacky sack", price: 5 },
+        {title: "Gold fidgit spinner", price: 2000 },
+        {title: "A second Tesla Model S", price: 90000 }
+    ];
+    
+    const totalPrices = wishlist.reduce((total, item) => total + item.price, 0);
+    
+    console.log(" C extra credit 1 ", totalPrices)
+    
+    
+    
+    const arrays = [["1", "2", "3"],[true], [4, 5, 6]];
+    
+    function flatten(arr){
+        const arraysFlat = arr.flat();
+        return arraysFlat
+    }
+   
+    console.log(" C extra credit 2 ", flatten(arrays))
+   
+   
+   
+    function leastToGreatest(arr) {
+            const sorted = arr.sort(function(a, b) {
+                if (a > b) {
+                    return 1
+                } else {
+                    return -1
+                }
+            })
+            return sorted
+        }
+      
+        console.log(" D 1 ", leastToGreatest([1, 3, 5, 2, 90, 20]))
+    
+    
+    
+        function greatestToLeast(arr) {
+        const sorted = arr.sort(function (a, b) {
+            if (a < b) {
+                return 1
+            } else {
+                return -1
+            }
+        })
+        return sorted
+    }
+    console.log(" D 2 ", greatestToLeast([1, 3, 5, 2, 90, 20]))
+    
+    
+    
+    function lengthToShort(arr) {
+        const shortToLong = arr.sort(function (a, b) {
+            return a.length - b.length
+        });
+        return shortToLong
+    }
+   
+    console.log(" D 3 ", lengthToShort(["dog", "wolf", "by", "family", "eaten"]))
+   
+   
+   
+    function alphabetical(arr) {
+        return arr.sort()
+    };
+    
+    console.log(" D extra credit ", alphabetical(["dog", "wolf", "by", "family", "eaten"]))
+    
+    
+    
+    
+    
+    
--- week5/es6.js
+++ week5/es6.js
@@ -2,6 +2,7 @@
 {
     const name = "John"
     const age = 101
+    
     function runForLoop(pets) {
       let petObjects = []
       for (let i = 0; i < pets.length; i++) {
@@ -12,20 +13,29 @@
         } else {
           name = "spot"
         }
+       
         console.log("pet name: ", name)
         pet.name =  name
         petObjects.push(pet)
       }
+     
       console.log("man name: ", name)
       return petObjects
     }
+    
     runForLoop(["cat", "dog"],)
     }
-    // ES6  # 2  rewrite this .map() using arrow functions
+   
+   
+   
     const carrots = ["bright orange", "ripe", "rotten"]
+    
     const mapVegetables = (arr) => arr.map((carrot) => ({type: "carrot", name: carrot}));
-    console.log("#2 ", mapVegetables(carrots));
-    // ES6 # 3  rewrite this .filter() uning an arrow function:
+    
+    console.log(mapVegetables(carrots)," #2 ");
+   
+   
+   
      const people = [
         {
             name: "Princess Peach",
@@ -44,23 +54,38 @@
             friendly: false
         }
     ]
+   
     const filterForFriendly = (arr) => arr.filter((person) => person.friendly)
-    console.log("#3 ", filterForFriendly(people));
-    // ES6 # 4  Rewrite the following funtions to be arrow functions:
+    
+    console.log(filterForFriendly(people), " #3 ");
+  
+  
+  
+
     const doMathSum = (a, b) =>  a + b
+    
     const produceProduct = (a, b) => a * b
-    console.log("#4 ", produceProduct(5, 5))
-     // ES6 # 5
+   
+    console.log(produceProduct(5, 5), "#4")
+    
+    
+    
     {
      function printString(firstName = "Jane", lastName = "Doe", age = 100) {
          return `Hi ${firstName} ${lastName}, how does it feel to be ${age}?`
      }
-    console.log("#5 ", printString())
+    
+     console.log(printString(), "#5")
     } 
-     // ES6 # 6 use template litterals to build the string from #5
+   
+   
+   
     let printString = (firstName = "Jane", lastName = "Doe", age = 100) => `Hi ${firstName} ${lastName}, how does it feel to be ${age}?`
-    console.log("#6 ", printString("Jesse", "Hampton", 33))
-     // ES6 # 7  Use the shorthand syntax to make the following filter take up one line. Copy and paste the array to test it.
+    
+    console.log(printString(), "#6")
+   
+   
+   
     const animals = [
         {
             type: "dog",
@@ -79,14 +104,17 @@
             name: "Sparky"
         }
     ];
+    
     const filterForDogs = (arr) => arr.filter(animal => (animal.type === "dog") ? true : false)
+   
     console.log(filterForDogs(animals), "#7")
-     // ES6 # 
+    
+    
+    
     let greeting = (location = "Hawaii", name = "Janice") => `Hi ${name}! Welcome to ${location}. I hope you enjoy your stay. Please ask the president of ${location} if you need anything.`
-    console.log("#8", greeting())
-    white_check_mark
-    eyes
-    raised_hands
+   
+    console.log(greeting(), "#8")
+   
     
     
     
